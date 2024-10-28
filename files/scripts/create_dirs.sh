be4f0fda7975c15237c1ea3510fb9d438345461e
Michael Jones <yardquit@pm.me>
2024-10-28 16:08:56 +0100

    testing...

--- files/scripts/create_dirs.sh
+++ files/scripts/create_dirs.sh
@@ -1,3 +1,3 @@
 # make mount point
-mkdir -p /mnt/{sshfsmnt_,localmnt_}{1..5} && sudo chmod 777 /mnt/{sshfsmnt_,localmnt_}{1..5} && touch /mnt/{sshfsmnt_,localmnt_}{1..5}/no_mount
-mkdir -p /mnt/bitlockr_{1..2} && sudo chmod 777 /mnt/bitlockr_{1..2} && touch /mnt/bitlockr_{1..2}/no_mount
+mkdir -p /var/mnt/{sshfsmnt_,localmnt_}{1..5} && sudo chmod 777 /var/mnt/{sshfsmnt_,localmnt_}{1..5} && touch /var/mnt/{sshfsmnt_,localmnt_}{1..5}/no_mount
+mkdir -p /var/mnt/bitlockr_{1..2} && sudo chmod 777 /var/mnt/bitlockr_{1..2} && touch /var/mnt/bitlockr_{1..2}/no_mount
