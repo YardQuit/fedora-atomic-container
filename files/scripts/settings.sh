a9df2c01cca0693ea3232ee5c1d622850457a4f4
Michael Jones <yardquit@pm.me>
2024-10-29 00:01:13 +0100

    testing...

--- files/scripts/settings.sh
+++ files/scripts/settings.sh
@@ -6,8 +6,9 @@
 set -oue pipefail
 
 # make mount points
-mkdir -p /var/mnt/{sshfsmnt_,localmnt_,bitlockr_}{1..5} 
-chmod 777 /var/mnt/{sshfsmnt_,localmnt_,bitlockr_}{1..5} 
-touch /var/mnt/{sshfsmnt_,localmnt_,bitlockr_}{1..5}/no_mount
+mkdir /var/mnt/temporary
+# mkdir -p /var/mnt/{sshfsmnt_,localmnt_,bitlockr_}{1..5} 
+# chmod 777 /var/mnt/{sshfsmnt_,localmnt_,bitlockr_}{1..5} 
+# touch /var/mnt/{sshfsmnt_,localmnt_,bitlockr_}{1..5}/no_mount
 
 echo -e "[DEFAULT]\nignoreip = 127.0.0.1 ::1\nbantime = 3600\nfindtime = 600\nmaxretry = 5\n\n[sshd]\nenabled = true" > /etc/fail2ban/jail.local
