61be7db0c8130776d0216e74669e7eecb6f47e48
Michael Jones <yardquit@pm.me>
2024-10-28 16:16:49 +0100

    testing...

--- files/scripts/create_dirs.sh
+++ files/scripts/create_dirs.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 # make mount point
 mkdir -p /var/mnt/{sshfsmnt_,localmnt_}{1..5} && sudo chmod 777 /var/mnt/{sshfsmnt_,localmnt_}{1..5} && touch /var/mnt/{sshfsmnt_,localmnt_}{1..5}/no_mount
 mkdir -p /var/mnt/bitlockr_{1..2} && sudo chmod 777 /var/mnt/bitlockr_{1..2} && touch /var/mnt/bitlockr_{1..2}/no_mount
