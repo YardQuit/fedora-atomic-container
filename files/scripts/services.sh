8e62ea7bc4714e929e2896e5f54dec641c5e7cce
Michael Jones <yardquit@pm.me>
2024-10-29 00:31:37 +0100

    testing...

---
+++ files/scripts/services.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -oue pipefail
+
+for drv in qemu interface network nodedev nwfilter secret storage; do
+    sudo systemctl enable virt${drv}d.service;
+    sudo systemctl enable virt${drv}d{,-ro,-admin}.socket;
+done
--- files/scripts/settings.sh
+++ files/scripts/settings.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env bash
-
-# Tell this script to exit if there are any errors.
-# You should have this in every custom script, to ensure that your completed
-# builds actually ran successfully without any errors!
 set -oue pipefail
-
-# make mount points
-mkdir /var/mnt/temporary
-# mkdir -p /var/mnt/{sshfsmnt_,localmnt_,bitlockr_}{1..5} 
-# chmod 777 /var/mnt/{sshfsmnt_,localmnt_,bitlockr_}{1..5} 
-# touch /var/mnt/{sshfsmnt_,localmnt_,bitlockr_}{1..5}/no_mount
 
 echo -e "[DEFAULT]\nignoreip = 127.0.0.1 ::1\nbantime = 3600\nfindtime = 600\nmaxretry = 5\n\n[sshd]\nenabled = true" > /etc/fail2ban/jail.local
