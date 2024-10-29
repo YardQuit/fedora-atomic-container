856ebeb89482aa32218d7eee47f96ab12358e348
Michael Jones <yardquit@pm.me>
2024-10-29 20:59:23 +0100

    testing...

--- files/scripts/settings.sh
+++ files/scripts/settings.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
+# configure fail2ban
 echo -e "[DEFAULT]\nignoreip = 127.0.0.1 ::1\nbantime = 3600\nfindtime = 600\nmaxretry = 5\n\n[sshd]\nenabled = true" > /etc/fail2ban/jail.local
+
+# install blue-build (bluebuild) command
+cargo install --locked blue-build
+bluebuild completions bash | tee /usr/share/bash-completion/completions/bluebuild
