ba243ec997326067968b02addcbc95aefe4ce10c
Michael Jones <yardquit@pm.me>
2024-10-30 15:03:57 +0100

    testing...

--- files/scripts/services.sh
+++ files/scripts/services.sh
@@ -6,6 +6,3 @@ for drv in qemu interface network nodedev nwfilter secret storage; do
     sudo systemctl enable virt${drv}d.service;
     sudo systemctl enable virt${drv}d{,-ro,-admin}.socket;
 done
-
-# enable rpm-ostree autoupgrade
-systemctl enable rpm-ostreed-automatic.timer
