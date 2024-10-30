212ccb1f4f0885206a659ffb92573567cd6653dd
Michael Jones <yardquit@pm.me>
2024-10-30 02:04:01 +0100

    testing...

--- files/scripts/services.sh
+++ files/scripts/services.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
+# enable virtualization services
 for drv in qemu interface network nodedev nwfilter secret storage; do
     sudo systemctl enable virt${drv}d.service;
     sudo systemctl enable virt${drv}d{,-ro,-admin}.socket;
 done
+
+# enable rpm-ostree autoupgrade
+systemctl enable rpm-ostreed-automatic.timer
--- files/scripts/settings.sh
+++ files/scripts/settings.sh
@@ -4,6 +4,5 @@ set -oue pipefail
 # configure fail2ban
 echo -e "[DEFAULT]\nignoreip = 127.0.0.1 ::1\nbantime = 3600\nfindtime = 600\nmaxretry = 5\n\n[sshd]\nenabled = true" > /etc/fail2ban/jail.local
 
-# install blue-build (bluebuild) command
-# cargo install --locked blue-build
-# bluebuild completions bash | tee /usr/share/bash-completion/completions/bluebuild
+# enable yubikey challange-response for sudo
+# auth       required     pam_yubico.so mode=challenge-response >> /etc/pam.d/sudo
